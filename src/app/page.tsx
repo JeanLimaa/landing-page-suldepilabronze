6ab06b076b4d6cc084e73bd576a031e37f0fdd2f
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2023-10-12 12:06:02 -0300

    responsive jobs section + implementation header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Services from "@/components/ServicesComponents/Services";
 import Certifications from "@/components/CertificationsComponents/Certifications";
 import Footer from "@/components/Footer";
 import Jobs from "@/components/JobsComponents/Jobs";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <HomeScreen />
       <Services />
       <Jobs />
